994399da3639fde1e8d73f61fc5c43c111d77bb5
PCPerso <jeremie.neotic@free.fr>
2024-09-11 17:12:11 +0200

    Membres des classes Model

--- app/src/main/java/com/openclassrooms/p15_eventorias/model/Event.kt
+++ app/src/main/java/com/openclassrooms/p15_eventorias/model/Event.kt
@@ -1,10 +1,34 @@
 package com.openclassrooms.p15_eventorias.model
 
+import java.util.Date
+
 data class Event (
 
     /**
      * Unique identifier for the event.
      */
-    val id: String = ""
+    val id: String = "",
+
+    val sTitle : String = "",
+
+    val sDescription : String = "",
+
+    // pas de champ Datetime dans Firebase => Long
+    val lDatetime : Long = 0,
+
+    /**
+     * URL (Firestore) de l'image de l'évènement.
+     */
+    val sURLEventPicture : String = "",
+
+    /**
+     * Adresse de l'évènement.
+     */
+    val sAdress : String = "", // TODO Denis : Type ?
+
+    /**
+     * User object representing the creator of the event.
+     */
+    val userCreatorEvent : User? = null,
 
 )
---
+++ app/src/main/java/com/openclassrooms/p15_eventorias/model/User.kt
@@ -0,0 +1,26 @@
+package com.openclassrooms.p15_eventorias.model
+
+data class User(
+    /**
+     * Unique identifier for the User.
+     */
+    val id: String = "",
+
+    /**
+     * User's name.
+     */
+    val sName: String = "",
+
+
+    /**
+     * User's email.
+     */
+    val sEmail: String = "",
+
+
+    /**
+     * avatar.
+     */
+    val sURLAvatar : String = "",
+
+)
